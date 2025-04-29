159e2140dc931c04a3837acf2fa0fbf13ec799f4
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-04-29 12:28:53 +0530

    Navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import arrow from "../../assets/arrow_icon.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select>
+          <option value="usd">USD</option>
+          <option value="euro">EUR</option>
+          <option value="inr">INR</option>
+        </select>
+        <button>
+          Sign Up <img src={arrow} alt="arrow_icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
