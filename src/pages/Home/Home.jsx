9ef7c26075710b25b93e3d5be462f8cefa0ce4a9
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-04-29 12:52:15 +0530

    Added routing

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="hero">
+        <h1>
+          Largest <br /> Crypto Marketplace
+        </h1>
+        <p>
+          Welcome to the world's larget cryptocurrency marketplace. Sign up to
+          explore more about cryptocurrency
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
