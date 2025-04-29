be9ef84d72499fac678958396d2b5c481b5652df
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-04-29 15:57:06 +0530

    Currency Table done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+  const { allCoins, currency } = useContext(CoinContext);
+  const [displayCoins, setDisplayCoins] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoins(allCoins);
+  }, [allCoins]);
+
   return (
     <div className="home">
       <div className="hero">
@@ -26,6 +34,29 @@ const Home = () => {
           <p style={{ textAlign: "center" }}>24H Change</p>
           <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoins.slice(0, 10).map((item, index) => (
+          <div key={index} className="table-layout">
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="" />
+              <p>{item.name + "- " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol}
+              {item.current_price.toLocaleString()}
+            </p>
+            <p
+              style={{ textAlign: "center" }}
+              className={item.price_change_percentage_24h > 0 ? "green" : "red"}
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
+            <p className="market-cap">
+              {currency.symbol}
+              {item.market_cap.toLocaleString()}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
